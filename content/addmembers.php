e1d846dff98dc7351e6949d7bb8d13108e452b78
hs-tharb <t.harbring@hemp-share.de>
2021-08-27 19:31:52 +0200

    added adding of members-page, minor css adjustments

---
+++ content/addmembers.php
@@ -0,0 +1,74 @@
+<div class="row">
+    <h1 class="font-header">Add A Member</h1>
+</div>
+<div class="row member-table">
+    <form action="/functions/addMembers.php" method="post">
+        <div class="row justify-content-center">
+            <div class="form-floating floatlabel">
+                <input type="text" class="form-control" id="name" placeholder="Character Name">
+                <label for="name">Character Name</label>
+            </div>
+        </div>
+        <br>
+        <div class="row justify-content-evenly">
+            <div class="col-6">
+                <div class="form-floating floatlabel">
+                    <select class="form-select" id="primaryjob">
+                        <option value="PLD">PLD</option>
+                        <option value="WAR">WAR</option>
+                        <option value="DRK">DRK</option>
+                        <option value="GNB">GNB</option>
+                        <option value="WHM">WHM</option>
+                        <option value="SCH">SCH</option>
+                        <option value="AST">AST</option>
+                        <option value="SGE">SGE</option>
+                        <option value="MNK">MNK</option>
+                        <option value="DRG">DRG</option>
+                        <option value="NIN">NIN</option>
+                        <option value="SAM">SAM</option>
+                        <option value="RPR">RPR</option>
+                        <option value="BRD">BRD</option>
+                        <option value="MCH">MCH</option>
+                        <option value="DNC">DNC</option>
+                        <option value="BLM">BLM</option>
+                        <option value="SMN">SMN</option>
+                        <option value="RDM">RDM</option>
+                    </select>
+                    <label for="primaryjob">Primary Job</label>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-floating floatlabel">
+                    <select class="form-select" id="secondaryjob">
+                        <option value="PLD">PLD</option>
+                        <option value="WAR">WAR</option>
+                        <option value="DRK">DRK</option>
+                        <option value="GNB">GNB</option>
+                        <option value="WHM">WHM</option>
+                        <option value="SCH">SCH</option>
+                        <option value="AST">AST</option>
+                        <option value="SGE">SGE</option>
+                        <option value="MNK">MNK</option>
+                        <option value="DRG">DRG</option>
+                        <option value="NIN">NIN</option>
+                        <option value="SAM">SAM</option>
+                        <option value="RPR">RPR</option>
+                        <option value="BRD">BRD</option>
+                        <option value="MCH">MCH</option>
+                        <option value="DNC">DNC</option>
+                        <option value="BLM">BLM</option>
+                        <option value="SMN">SMN</option>
+                        <option value="RDM">RDM</option>
+                    </select>
+                    <label for="primaryjob">Secondary Job</label>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <button type="submit" class="btn btn-light">Add Member</button>
+            </div>
+        </div>
+    </form>
+</div>
--- content/members.php
+++ content/members.php
@@ -15,56 +15,65 @@
             <tbody>
                 <tr>
                     <th scope="row">Tank</th>
-                    <td>Legiana Yukihime</td>
                     <td>
-                        <select class="form-select">
-                            <option value="1">PLD</option>
-                            <option value="2">WAR</option>
-                            <option value="3">DRK</option>
-                            <option value="4">GNB</option>
-                            <option value="5">WHM</option>
-                            <option value="6">SCH</option>
-                            <option value="7">AST</option>
-                            <option value="8">SGE</option>
-                            <option value="9">MNK</option>
-                            <option value="10">DRG</option>
-                            <option value="11">NIN</option>
-                            <option value="12">SAM</option>
-                            <option value="13">RPR</option>
-                            <option value="14" selected>BRD</option>
-                            <option value="15">MCH</option>
-                            <option value="16">DNC</option>
-                            <option value="17">BLM</option>
-                            <option value="18">SMN</option>
-                            <option value="19">RDM</option>
+                        <input type="text" class="form-control" id="name" value="Legiana Yukihime">
+                    </td>
+                    <td>
+                        <select class="form-select" id="primaryjob">
+                            <option value="PLD">PLD</option>
+                            <option value="WAR">WAR</option>
+                            <option value="DRK">DRK</option>
+                            <option value="GNB">GNB</option>
+                            <option value="WHM">WHM</option>
+                            <option value="SCH">SCH</option>
+                            <option value="AST">AST</option>
+                            <option value="SGE">SGE</option>
+                            <option value="MNK">MNK</option>
+                            <option value="DRG">DRG</option>
+                            <option value="NIN">NIN</option>
+                            <option value="SAM">SAM</option>
+                            <option value="RPR">RPR</option>
+                            <option value="BRD" selected>BRD</option>
+                            <option value="MCH">MCH</option>
+                            <option value="DNC">DNC</option>
+                            <option value="BLM">BLM</option>
+                            <option value="SMN">SMN</option>
+                            <option value="RDM">RDM</option>
                         </select>
                     </td>
                     <td>
-                        <select class="form-select">
-                            <option value="1">PLD</option>
-                            <option value="2">WAR</option>
-                            <option value="3">DRK</option>
-                            <option value="4">GNB</option>
-                            <option value="5" selected>WHM</option>
-                            <option value="6">SCH</option>
-                            <option value="7">AST</option>
-                            <option value="8">SGE</option>
-                            <option value="9">MNK</option>
-                            <option value="10">DRG</option>
-                            <option value="11">NIN</option>
-                            <option value="12">SAM</option>
-                            <option value="13">RPR</option>
-                            <option value="14">BRD</option>
-                            <option value="15">MCH</option>
-                            <option value="16">DNC</option>
-                            <option value="17">BLM</option>
-                            <option value="18">SMN</option>
-                            <option value="19">RDM</option>
+                        <select class="form-select" id="secondaryjob">
+                            <option value="PLD">PLD</option>
+                            <option value="WAR">WAR</option>
+                            <option value="DRK">DRK</option>
+                            <option value="GNB">GNB</option>
+                            <option value="WHM" selected>WHM</option>
+                            <option value="SCH">SCH</option>
+                            <option value="AST">AST</option>
+                            <option value="SGE">SGE</option>
+                            <option value="MNK">MNK</option>
+                            <option value="DRG">DRG</option>
+                            <option value="NIN">NIN</option>
+                            <option value="SAM">SAM</option>
+                            <option value="RPR">RPR</option>
+                            <option value="BRD">BRD</option>
+                            <option value="MCH">MCH</option>
+                            <option value="DNC">DNC</option>
+                            <option value="BLM">BLM</option>
+                            <option value="SMN">SMN</option>
+                            <option value="RDM">RDM</option>
                         </select>
                     </td>
                 </tr>
             </tbody>
         </table>
-        <button type="submit" class="btn btn-light">Save Changes</button>
+        <div class="row justify-content-evenly">
+            <div class="col-auto">
+                <a class="btn btn-light" href="./index.php?page=addmembers">Add Member</a>
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-light">Save Changes</button>
+            </div>
+        </div>
     </form>
 </div>
