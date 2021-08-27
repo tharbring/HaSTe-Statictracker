446aff919c3b069f8f57b7b7739b94dbac61b5c5
hs-tharb <t.harbring@hemp-share.de>
2021-08-27 17:40:10 +0200

    added addmembers-routing

--- includes/content.php
+++ includes/content.php
@@ -2,8 +2,12 @@
 
 /* Variablen */
 $members = "members";
+$addmembers = "addmembers";
 
 switch ($_GET['page']) {
+    case $addmembers:
+        include ($_SERVER['DOCUMENT_ROOT'] . '/content/addmembers.php');
+    break;
     case $members:
        include ($_SERVER['DOCUMENT_ROOT'] . '/content/members.php'); 
     break;
