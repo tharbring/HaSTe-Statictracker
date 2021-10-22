389c953caa50e755ec0a9591a1a358113d57ac1b
hs-tharb <t.harbring@hemp-share.de>
2021-10-23 00:17:50 +0200

    added back-buttons to detailed overview

--- content/timesdetail.php
+++ content/timesdetail.php
@@ -2,6 +2,12 @@
     <h1 class="font-header">Detailed Timetable</h1>
 </div>
 <br>
+<div class="row justify-content-evenly">
+    <div class="col-auto">
+        <a class="btn btn-light" href="./index.php">Back To Overview</a>
+    </div>
+</div>
+<br>
 <div class="row timesdetail-table">
     <table class="table table-dark table-bordered table-sm border-dark">
         <thead>
@@ -13,5 +19,11 @@
             <?php include ($_SERVER['DOCUMENT_ROOT'] . '/functions/showDetailedTimetableContent.php'); ?>
         </tbody>
     </table>
-
 </div>
+<br>
+<div class="row justify-content-evenly">
+    <div class="col-auto">
+        <a class="btn btn-light" href="./index.php">Back To Overview</a>
+    </div>
+</div>
+<br>
