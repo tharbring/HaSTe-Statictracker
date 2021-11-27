b4cfbd4e3c20392cd41f4129a472fdee87fada87
hs-tharb <t.harbring@hemp-share.de>
2021-11-27 19:19:27 +0100

    upload adjustments

--- content/start.php
+++ content/start.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <!--<h1 class="font-header">Scree Family</h1>-->
+    <!--<h1 class="font-header">Paraselene</h1>-->
     <h1 class="font-header">Statictracker-Demo</h1>
 </div>
 <br>
